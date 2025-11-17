d76acdcc5ed7faced4d994be614002bc1e075263
Liu <93741427+neuropunk87@users.noreply.github.com>
2025-11-17 22:10:07 +0200

    Changed server port and updated cookie secret usage

--- clothica-shop/src/app.ts
+++ clothica-shop/src/app.ts
@@ -1,12 +1,11 @@
 import express from 'express';
 import AdminJS from 'adminjs';
 import { buildAuthenticatedRouter } from '@adminjs/express';
-
 import provider from './admin/auth-provider.js';
 import options from './admin/options.js';
 import initializeDb from './db/index.js';
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3030;
 
 const start = async () => {
   const app = express();
@@ -20,17 +19,16 @@ const start = async () => {
   } else {
     admin.watch();
   }
-
   const router = buildAuthenticatedRouter(
     admin,
     {
-      cookiePassword: process.env.COOKIE_SECRET,
+      cookiePassword: process.env.ADMIN_COOKIE_SECRET,
       cookieName: 'adminjs',
       provider,
     },
     null,
     {
-      secret: process.env.COOKIE_SECRET,
+      secret: process.env.ADMIN_COOKIE_SECRET,
       saveUninitialized: true,
       resave: true,
     },
@@ -39,7 +37,8 @@ const start = async () => {
   app.use(admin.options.rootPath, router);
 
   app.listen(port, () => {
-    console.log(`AdminJS available at http://localhost:${port}${admin.options.rootPath}`);
+    const baseUrl = process.env.RENDER_EXTERNAL_URL || `http://localhost:${port}`;
+    console.log(`AdminJS available at ${baseUrl}${admin.options.rootPath}`);
   });
 };
 
