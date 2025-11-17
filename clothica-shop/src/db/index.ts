32e7b16390dba194ac0882c9a3e7f8954723e6c1
Liu <93741427+neuropunk87@users.noreply.github.com>
2025-11-17 22:11:42 +0200

    Changed database connection URL environment variable

--- clothica-shop/src/db/index.ts
+++ clothica-shop/src/db/index.ts
@@ -5,7 +5,7 @@ import AdminJS from 'adminjs';
 AdminJS.registerAdapter({ Database, Resource });
 
 const initialize = async () => {
-  const db = await mongoose.connect(process.env.DATABASE_URL as string);
+  const db = await mongoose.connect(process.env.MONGO_URL as string);
 
   return { db };
 };
