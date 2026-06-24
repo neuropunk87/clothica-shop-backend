aad279747199df92e926d434f8d1b446ca42313c
Liu <93741427+neuropunk87@users.noreply.github.com>
2026-06-24 09:24:26 +0300

    fix(categories): accept category ids in goods filters

--- src/middleware/processCategoryFilter.js
+++ src/middleware/processCategoryFilter.js
@@ -1,3 +1,5 @@
+import mongoose, { isValidObjectId } from 'mongoose';
+import slugify from 'slugify';
 import { Category } from '../models/category.js';
 
 export const processCategoryFilter = async (req, res, next) => {
@@ -5,10 +7,37 @@ export const processCategoryFilter = async (req, res, next) => {
     return next();
   }
   try {
-    const categorySlug = req.query.category;
-    const category = await Category.findOne({ slug: categorySlug })
+    const categoryFilter = Array.isArray(req.query.category)
+      ? req.query.category[0]
+      : req.query.category;
+
+    if (!categoryFilter) {
+      delete req.query.category;
+      return next();
+    }
+
+    if (isValidObjectId(categoryFilter)) {
+      req.query.category = categoryFilter;
+      return next();
+    }
+
+    const categorySlug = slugify(categoryFilter, {
+      lower: true,
+      strict: true,
+      locale: 'uk',
+    });
+    const category = await Category.findOne(
+      mongoose.trusted({
+        $or: [
+          { slug: categoryFilter },
+          { slug: categorySlug },
+          { name: categoryFilter },
+        ],
+      }),
+    )
       .select('_id')
       .lean();
+
     if (category) {
       req.query.category = category._id.toString();
     } else {
